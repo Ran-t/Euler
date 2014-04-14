7410b8f688bc40bd93e22b54e6ec72ffb91333ae
Randall <Randall_T@outlook.com>
2014-04-13 23:04:17 -0400

    Removed now ignored files.

--- Haskell/2/solution.hs
+++
@@ -1,8 +0,0 @@
--- this program calculates the sum of all even Fibonacci numbers
--- whose values do not exceed four million.
-
--- function generating the Fibonacci numbers.
-fibbs = 1 : 2 :    fibbs' 1 2
-fibbs' x y = x+y : fibbs' y (x+y)
-
-main = print . sum . takeWhile (<4000000) . filter even $ fibbs
